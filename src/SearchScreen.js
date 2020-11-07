1b03fb9dc16dedd4b93fa24ceab8550a2f87f2c3
abrarbhat <abrarbhat008@gmail.com>
2020-11-07 15:46:27 +0530

    Added Authentication token in project

--- src/SearchScreen.js
+++ src/SearchScreen.js
@@ -1,17 +1,23 @@
 import React,{useState} from 'react';
 import {View,Text,StyleSheet} from 'react-native';
 import SearchBar from '../components/SearchBar';
+import useResults from '../hooks/useResults';
+
 
 const SearchScreen =()=>{
     const [term,setTerm]= useState('')
+    const [searchApi,results,errorMessage]= useResults();
+   
     return(
         <View style>
-            <SearchBar term ={term} 
-            onTermChange={newTerm=>setTerm(newTerm)}
-            onTermSubmit={()=>console.log("Term Was Submitted")}
+            <SearchBar
+            term ={term} 
+            onTermChange={setTerm}
+            onTermSubmit={searchApi(term)}
             />
+            {errorMessage?<Text>{errorMessage}</Text>:null}
             <Text>Search Screen</Text>
-            <Text>{term}</Text>
+            <Text>We have found {results.length} results</Text>
         </View>
     );
 };
